a2b86e7c537ca3bbbc76ee8148c452848e84200a
Vinicius Holtman <viniholtman28@gmail.com>
2023-02-01 18:05:40 -0300

    Create tratament error

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,23 @@
 import * as express from "express";
+import "express-async-errors"
 import { routes } from "./routes/routes"
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use((err: Error, request: express.Request, response: express.Response, next: express.NextFunction) => {
+  if (err instanceof Error) {
+    return response.status(400).json({
+      message: err.message
+    })
+  }
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error"
+  })
+})
 
 app.listen(3000, () => console.log("Server is running! 🤖"))
