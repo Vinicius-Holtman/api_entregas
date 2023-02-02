5f1ddabf268e75c11006adc8c2bbf85847bebefc
Vinicius Holtman <viniholtman28@gmail.com>
2023-02-02 11:17:57 -0300

    Create delivery

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -3,6 +3,7 @@ import { CreateClientController } from "../modules/clients/useCases/createClient
 import { AuthenticateClientController } from "../modules/accounts/authenticateUser/AuthenticateClientController";
 import { CreateDeliverymanController } from "../modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
 import { AuthenticateDeliverymanController } from "../modules/accounts/authenticateDeliveryman/AuthenticateDeliverymanController";
+import { CreateDeliveryController } from "../modules/deliveries/useCases/createDelivery/CreateDeliveryController";
 
 const routes = Router()
 
@@ -10,10 +11,13 @@ const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
 const authenticateDeliverymanController = new AuthenticateDeliverymanController()
 const deliverymanController = new CreateDeliverymanController()
+const deliveryController = new CreateDeliveryController()
 
 routes.post("/client", createClientController.handle)
 routes.post("/client/authenticate", authenticateClientController.handle)
 routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
 routes.post("/deliveryman", deliverymanController.handle)
+
+routes.post("/delivery", deliveryController.handle)
 
 export { routes }
