261026d3995a771bfd6125aa789f4d100355d2ed
Vinicius Holtman <viniholtman28@gmail.com>
2023-02-02 10:19:44 -0300

    Create authenticate to deliveryman

---
+++ src/modules/accounts/authenticateDeliveryman/AuthenticateDeliverymanController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from "express"
+import { AuthenticateDeliverymanUseCase } from "./AutheticateDeliverymanUseCase"
+
+export class AuthenticateDeliverymanController {
+  async handle(req: Request, res: Response) {
+    const { username, password } = req.body
+
+    const authenticateDeliverymanUseCase = new AuthenticateDeliverymanUseCase()
+
+    const result = await authenticateDeliverymanUseCase.execute({ username, password })
+
+    return res.json(result)
+  }
+}
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,19 @@
 import { Router } from "express";
 import { CreateClientController } from "../modules/clients/useCases/createClient/CreateClientController";
 import { AuthenticateClientController } from "../modules/accounts/authenticateUser/AuthenticateClientController";
+import { CreateDeliverymanController } from "../modules/deliveryman/useCases/createDeliveryman/CreateDeliverymanController";
+import { AuthenticateDeliverymanController } from "../modules/accounts/authenticateDeliveryman/AuthenticateDeliverymanController";
 
 const routes = Router()
 
 const createClientController = new CreateClientController()
 const authenticateClientController = new AuthenticateClientController()
+const authenticateDeliverymanController = new AuthenticateDeliverymanController()
+const deliverymanController = new CreateDeliverymanController()
 
 routes.post("/client", createClientController.handle)
-routes.post("/authenticate", authenticateClientController.handle)
+routes.post("/client/authenticate", authenticateClientController.handle)
+routes.post("/deliveryman/authenticate", authenticateDeliverymanController.handle)
+routes.post("/deliveryman", deliverymanController.handle)
 
 export { routes }
